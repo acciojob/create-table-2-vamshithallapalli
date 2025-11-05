13a1b4f234b346acc3942a276511a0f17e71f05a
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-11-05 17:11:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function createTable() {
     //Write your code here
-  
+    let rows = prompt('enter no of rows');
+	let cols = prompt('enter no of cols');
+
+	rows = Number(rows);
+	cols = Number(cols);
+
+	if(isNaN(rows) || isNaN(cols)){
+		return;
+	}
+
+	if(rows<=0 || cols<=0){
+		 alert("Please enter positive numbers greater than 0.");
+        return;
+	}
+
+	const table = document.getElementById('myTable');
+	table.innerHTML = '';
+
+	for(let i=0; i<rows; i++){
+		let row = table.insertRow();
+
+		for(let j=0; j<cols; j++){
+			let cell = row.insertCell();
+			cell.innerHTML = `Row-${i} Column-${j}`
+		}
+	}
 }
